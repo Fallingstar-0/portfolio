19d01494ae16d9896a588e0da0de947489bcc037
Tony Hopkins <syiehya50@gmail.com>
2024-04-03 01:40:09 +0200

    updated css

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -19,8 +19,6 @@ const Hero = () => {
         alt="Hero Image of me"
         className={styles.heroImg}
       />
-      <div className={styles.topBlur} />
-      <div className={styles.bottomBlur} />
     </section>
   )
 }
--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -4,41 +4,59 @@ import { getImageUrl } from "../../utils"
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false)
+  const [fix, setFix] = useState(false)
+
+  const changeBackground = () => {
+    if (window.scrollY >= 113) {
+      setFix(true)
+    } else {
+      setFix(false)
+    }
+  }
+
+  window.addEventListener("scroll", changeBackground)
+
   return (
-    <nav className={styles.navbar}>
-      <a className={styles.title} href="/">
-        Portfolio
-      </a>
-      <div className={styles.menu}>
-        <img
-          className={styles.menuBtn}
-          src={
-            menuOpen
-              ? getImageUrl("nav/closeIcon.png")
-              : getImageUrl("nav/menuIcon.png")
-          }
-          alt="menu-button"
-          onClick={() => setMenuOpen(!menuOpen)}
-        />
-        <ul
-          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-          onClick={() => setMenuOpen(false)}
-        >
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#experience">Experience</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div className={styles.container}>
+      <nav
+        className={
+          fix ? `${styles.navbar} ${styles.active}` : `${styles.navbar}`
+        }
+      >
+        <a className={styles.title} href="/">
+          TONY HOPKINS
+        </a>
+        <div className={styles.menu}>
+          <img
+            className={styles.menuBtn}
+            src={
+              menuOpen
+                ? getImageUrl("nav/closeIcon.png")
+                : getImageUrl("nav/menuIcon.png")
+            }
+            alt="menu-button"
+            onClick={() => setMenuOpen(!menuOpen)}
+          />
+          <ul
+            className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+            onClick={() => setMenuOpen(false)}
+          >
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   )
 }
 
